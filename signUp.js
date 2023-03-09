aecec9c95717303aa1e0ad32964a9bb3156f7d3c
cynthia-Kam <abijurucyn@gmail.com>
2023-03-09 15:54:05 +0200

    integration:signup with localhost

--- signUp.js
+++ signUp.js
@@ -2,35 +2,36 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
     const username = document.getElementById('username');
-    let password = document.getElementById('password');
-    let password2 = document.getElementById('password2');
-    const submit=document.getElementById('submit');
-    const success=document.getElementById('success-signup');
-    
-    
-    form.addEventListener('submit', e=> {
-    e.preventDefault();
-    validateInputs();
-    adduser();
-    
+    const fullname = document.getElementById('fullname');
+    const password = document.getElementById('password');
+    const password2 = document.getElementById('password2');
+    const submit = document.getElementById('submit');
+    const success = document.getElementById('success-signup');
+
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        validateInputs();
+        adduser();
+
     });
     // submit.addEventListener('click',e=>{
     // e.preventDefault();
     // adduser();
     // })
 
-    const setError =(element,message)=>{
-        inputControl=element.parentElement;
-        const errorDisplay=inputControl.querySelector('.error');
-        errorDisplay.innerText= message;
+    const setError = (element, message) => {
+        inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+        errorDisplay.innerText = message;
         inputControl.classList.add('error');
         inputControl.classList.remove('success');
     }
-    const setSuccess=element=>{
-        let Status=false
-        const inputControl= element.parentElement;
-        const errorDisplay=inputControl.querySelector('.error');
-        errorDisplay.innerText="";
+    const setSuccess = element => {
+        let Status = false
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+        errorDisplay.innerText = "";
         inputControl.classList.remove('error');
         // inputControl.classList.add('success');
     }
@@ -38,100 +39,117 @@ document.addEventListener('DOMContentLoaded', () => {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     }
-    const validateInputs=()=>{
-        let usernameValue=username.value.trim();
-        let passwordValue=password.value.trim();
-        let password2Value=password2.value.trim();
+    const validateInputs = () => {
+        let fullnameValue = fullname.value.trim();
+        let usernameValue = username.value.trim();
+        let passwordValue = password.value.trim();
+        let password2Value = password2.value.trim();
+        if (fullnameValue === '') {
+            setError(fullname, "Name is required")
+        }
+        else {
+            setSuccess(fullname);
 
-        if(usernameValue ===''){
-            setError(username,"Email is required")
         }
-        else if(!isValidEmail(usernameValue)){
-            setError(username,"provide a valid email address")
+        if (usernameValue === '') {
+            setError(username, "Email is required")
         }
-        else{
+        else if (!isValidEmail(usernameValue)) {
+            setError(username, "provide a valid email address")
+        }
+        else {
             setSuccess(username);
-           
+
         }
-        if(passwordValue===''){
-            setError(password,"password can't be empty")   
+        if (passwordValue === '') {
+            setError(password, "password can't be empty")
         }
-        else{
-            if(password.value.length<8){
-                setError(password,"password must have atleast 8 characters")
+        else {
+            if (password.value.length < 8) {
+                setError(password, "password must have atleast 8 characters")
             }
             setSuccess(password);
         }
-        if(password2Value===''){
-            setError(password2,"password field is required")
+        if (password2Value === '') {
+            setError(password2, "password field is required")
         }
-        else{
-            if(passwordValue!==password2Value){
-                setError(password2,"password does not match")  
-                    }
-                    else{
+        else {
+            if (passwordValue !== password2Value) {
+                setError(password2, "password does not match")
+            }
+            else {
                 setSuccess(password2);
-                    }
-        } 
+            }
+        }
     }
-    
-    const adduser=()=>{
-        let password2Value=password2.value.trim();
-        var usernameValue=username.value.trim();
-        var passwordValue=password.value.trim();
-        if(usernameValue===''||passwordValue===''||password2Value===''){
-                    validateInputs();     
+
+    const adduser = () => {
+        const fullnameValue = fullname.value;
+        const password2Value = password2.value;
+        const usernameValue = username.value;
+        const passwordValue = password.value;
+
+
+        if (usernameValue === '' || passwordValue === '' || password2Value === '') {
+            validateInputs();
+        }
+        else if (!isValidEmail(usernameValue)) {
+            validateInputs();
+        }
+        else if (passwordValue !== password2Value) {
+            validateInputs();
+        }
+        else if (fullnameValue === '') {
+            validateInputs();
+        }
+        else {
+            const data = { fullname:fullnameValue, email:usernameValue,password:passwordValue, isAdmin: false };
+            fetch('http://localhost:2000/signup', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+                
+            })
+            
+                .then((response) => 
+                response.json()
+                    // console.log (response)
+                )
+            .then((data) => {
+                console.log('Data:', data);
+                if (data.ok) {
+                    success.style.display='block'
+                    success.innerText=data.message
+                    password.value=''
+                    password2.value=''
+                } else {
+                    alert(data.errors)
                 }
-        else if(!isValidEmail(usernameValue)){
-            validateInputs();
+            })
+            .catch(error => alert(error))
+            console.log(data)
         }
-        else if(passwordValue!==password2Value){
-            validateInputs();
-        }
-                else{
-        users = JSON.parse(localStorage.getItem('users')) || [];
-        let targetUser= users.find(users=>users.email==usernameValue);
-        if(targetUser){
-            // success.style.backgroundColor= 'red'
-           setError(username,"account already exist")
-            password.value=''
-            password2.value=''  
-        }else{
-            let user = {};
-            user.email = usernameValue
-            user.password = passwordValue
-            user.password2= password2.value
-           
-        users.push(user);
-        const stringUsers = JSON.stringify(users);
-        localStorage.setItem('users', stringUsers);
-        success.style.display='block';
-        success.innerText="successfully created a new account!!"
-        console.log(users);
-        password.value=''
-        password2.value=''
-        console.log("users".users)
-          }  }
     }
 
     const togglePassword = document.querySelector('#togglePassword');
     togglePassword.addEventListener('click', function (e) {
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    this.classList.toggle('bi-eye');
-    
-});
-const togglePassword2 = document.querySelector('#togglePassword2');
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+        this.classList.toggle('bi-eye');
 
-
-  togglePassword2.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
-    password2.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('bi-eye');
-    
-});
     });
-    
-    
+    const togglePassword2 = document.querySelector('#togglePassword2');
+
+
+    togglePassword2.addEventListener('click', function (e) {
+        // toggle the type attribute
+        const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+        password2.setAttribute('type', type);
+        // toggle the eye slash icon
+        this.classList.toggle('bi-eye');
+
+    });
+});
+
