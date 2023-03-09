99c8c421b514f97ae075c69f27db2f32d2db699d
cynthia-Kam <abijurucyn@gmail.com>
2023-03-09 23:11:21 +0200

    integration:contact form

--- index.js
+++ index.js
@@ -4,28 +4,50 @@ document.addEventListener('DOMContentLoaded', () => {
     const fname = document.getElementById('fname');
     const email = document.getElementById('email');
     const mess = document.getElementById('message');
+    const success = document.getElementById('success-signup');
 
 
     window.addEventListener('load', () => {
-        Savedblogs = JSON.parse(localStorage.getItem('blogs')) || [];
         div = document.getElementById('blogsCardsDiv')
         var i =0;
+       // Savedblogs = JSON.parse(localStorage.getItem('blogs')) || [];
+       fetch('https://pink-thankful-oyster.cyclic.app/blogs')
+        .then(response => response.json())
+        .then(blogs => {
+            blogs.data.forEach((element,index) => {
+                let content = document.createElement('div');
+    
+                content.innerHTML = `
+                     
+                     <img src="${element.image}" alt="blog" style="width:100%">
+                     <p class="title" id="${(element._id)}">${(element.title)}</p>
+                     <a href="#"><i class="fa fa-thumbs-up">likes</i></a>
+                     <a href="#"><i class="fa fa-eye">views</i></a>
+     
+                     <p><button id="${i}" class="readMore">Read more</button></p>`
+    
+                div.appendChild(content)
+                i++;
+            });
+           
+        })
+        
         //if (div.hasChildNodes()) return div.innerHTML = "";
-        Savedblogs.forEach((element,index) => {
-            let content = document.createElement('div');
+        // Savedblogs.forEach((element,index) => {
+        //     let content = document.createElement('div');
 
-            content.innerHTML = `
+        //     content.innerHTML = `
                  
-                 <img src="${element.image}" alt="blog" style="width:100%">
-                 <p class="title" id="${(element.blogId)}">${(element.title)}</p>
-                 <a href="#"><i class="fa fa-thumbs-up">likes</i></a>
-                 <a href="#"><i class="fa fa-eye">views</i></a>
+        //          <img src="${element.image}" alt="blog" style="width:100%">
+        //          <p class="title" id="${(element.blogId)}">${(element.title)}</p>
+        //          <a href="#"><i class="fa fa-thumbs-up">likes</i></a>
+        //          <a href="#"><i class="fa fa-eye">views</i></a>
  
-                 <p><button id="${i}" class="readMore">Read more</button></p>`
+        //          <p><button id="${i}" class="readMore">Read more</button></p>`
 
-            div.appendChild(content)
-            i++;
-        });
+        //     div.appendChild(content)
+        //     i++;
+        // });
         
         var ButtonReadMore=document.querySelectorAll('.readMore')
         var ParagraphTitle=document.querySelectorAll('.title')
@@ -68,10 +90,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     }
-
+   
     const validateInputs = () => {
+        const nameValue=fname.value;
         const emailValue = email.value;
         const messageValue = mess.value;
+        
         if (emailValue === '') {
             setError(email, "email is required")
         }
@@ -91,18 +115,36 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const sendMessage = () => {
+        const nameValue= fname.value;
+        const emailValue = email.value;
+        const messageValue = mess.value;
 
         if (email.value === '' || mess.value === '' || !isValidEmail(email.value)) {
             validateInputs();
         }
         else {
-            messages = JSON.parse(localStorage.getItem('messages')) || [];
-            let user = {};
-            user.email = email.value
-            user.message = mess.value
-            messages.push(user);
-            const stringMessages = JSON.stringify(messages);
-            localStorage.setItem('messages', stringMessages);
+           
+            const data = { fullname:nameValue, email:emailValue,content:messageValue };
+            fetch('https://pink-thankful-oyster.cyclic.app/message', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)  
+            })
+                .then((response) => 
+                response.json()
+                    // console.log (response)
+                )
+            .then((data) => {
+             console.log(data.message)
+             success.style.display='block'
+             success.innerText=data.message
+             mess.value=''
+             email.value=''
+             fname.value=''
+
+            })
 
         }
 
