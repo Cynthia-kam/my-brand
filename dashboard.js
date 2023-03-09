569eba7a63260241311ae0cdfba80d543915d5d7
cynthia-Kam <abijurucyn@gmail.com>
2023-03-09 22:22:02 +0200

    integration:login and dashboard

--- dashboard.js
+++ dashboard.js
@@ -1,63 +1,70 @@
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function (event) {
     //do work
 
-            const divItem = document.getElementById('mainMessage');
-            const divItem2 = document.getElementById('mainMessage2');
-            const divItem3 = document.getElementById('mainMessage3');
-            const message = document.getElementById('message');
-            const reply = document.getElementById('reply');
-            const like= document.getElementById('like');
-            const userprofile=document.getElementById('userprofile');
-            totalMessages=document.getElementById('totalMessages');
-            totalBlogs=document.getElementById('totalBlogs');
-            totalUsers=document.getElementById('totalUsers');
-            superAdminEmail='abijurucyn@gmail.com'
-            const boxUsers=document.getElementById('boxUsers');
-            messageButton=document.getElementById('message-btn');
-             main_body=document.getElementById('mainbody');
-            logout=document.getElementById('logout');
+    const divItem = document.getElementById('mainMessage');
+    const divItem2 = document.getElementById('mainMessage2');
+    const divItem3 = document.getElementById('mainMessage3');
+    const message = document.getElementById('message');
+    const reply = document.getElementById('reply');
+    const like = document.getElementById('like');
+    const userprofile = document.getElementById('userprofile');
+    totalMessages = document.getElementById('totalMessages');
+    totalBlogs = document.getElementById('totalBlogs');
+    totalUsers = document.getElementById('totalUsers');
+    superAdminEmail = 'abijurucyn@gmail.com'
+    const boxUsers = document.getElementById('boxUsers');
+    messageButton = document.getElementById('message-btn');
+    main_body = document.getElementById('mainbody');
+    logout = document.getElementById('logout');
 
-            document.getElementById('ham-menu').addEventListener('click',()=>{
-               if(document.getElementById('sidebar').style.display='none'){
-                document.getElementById('sidebar').style.backgroundColor='black'
-                document.getElementById('sidebar').style.border='none'
-                document.getElementById('sidebar').style.display='block'
-               }
-               main_body.addEventListener('click',()=>{
-                if(document.getElementById('sidebar').style.display='block'){
-                    document.getElementById('sidebar').style.display='none'
-                }
-               })
-            })
-           document.getElementById('ShowBlogButton').addEventListener('click',()=>{
-            main_body.style.display='block'
-           })
-
-            logout.addEventListener('click',e=>{
-                localStorage.removeItem('currentUser');
-                window.location="/login.html"
-            })
-            Saveduser = JSON.parse(localStorage.getItem('currentUser')) || [];
-            if(Saveduser.email!='abijurucyn@gmail.com'){
-                messageButton.style.display='none';
+    document.getElementById('ham-menu').addEventListener('click', () => {
+        if (document.getElementById('sidebar').style.display = 'none') {
+            document.getElementById('sidebar').style.backgroundColor = 'black'
+            document.getElementById('sidebar').style.border = 'none'
+            document.getElementById('sidebar').style.display = 'block'
+        }
+        main_body.addEventListener('click', () => {
+            if (document.getElementById('sidebar').style.display = 'block') {
+                document.getElementById('sidebar').style.display = 'none'
             }
-            window.addEventListener('load', e=> {
-                if(Saveduser.email==='abijurucyn@gmail.com'){userprofile.innerText='superAdmin'}
-                else{userprofile.innerText='Admin'}
-                totalUsers.innerText=JSON.parse(localStorage.users).length;
-                totalBlogs.innerText=JSON.parse(localStorage.blogs).length;
-                totalMessages.innerText=JSON.parse(localStorage.messages).length;
-                console.log(JSON.parse(localStorage.users).length)
-              //  bodydiv.style.display='none'
-                });
-            
-            boxUsers.addEventListener('click',()=>{
-                SavedUsers = JSON.parse(localStorage.getItem('users')) || []; 
-                if (main_body.hasChildNodes()) return main_body.innerHTML = "";
-                SavedUsers.forEach(element=>{
-                    let content = document.createElement('div');
-                    content.innerHTML=`
+        })
+    })
+    document.getElementById('ShowBlogButton').addEventListener('click', () => {
+        main_body.style.display = 'block'
+    })
+
+    logout.addEventListener('click', e => {
+        localStorage.removeItem('currentUser');
+        window.location = "/login.html"
+    })
+    Saveduser = JSON.parse(localStorage.getItem('currentUser')) || [];
+    
+    window.addEventListener('load', e => {
+        fetch('https://pink-thankful-oyster.cyclic.app/blogs')
+        .then(response => response.json())
+        .then(blogs => {
+            totalBlogs.innerText=blogs.data.length
+        })
+        fetch('https://pink-thankful-oyster.cyclic.app/message')
+        .then(response => response.json())
+        .then(messages => {
+            totalMessages.innerText=messages.data.length
+        })
+
+        if (Saveduser.email === 'abijurucyn@gmail.com') { userprofile.innerText = 'superAdmin' }
+        else { userprofile.innerText = 'Admin' }
+        totalUsers.innerText = JSON.parse(localStorage.users).length;
+        console.log(JSON.parse(localStorage.users).length)
+        //  bodydiv.style.display='none'
+    });
+
+    boxUsers.addEventListener('click', () => {
+        SavedUsers = JSON.parse(localStorage.getItem('users')) || [];
+        if (main_body.hasChildNodes()) return main_body.innerHTML = "";
+        SavedUsers.forEach(element => {
+            let content = document.createElement('div');
+            content.innerHTML = `
                    
                     <div class="main-content" id="mainMessage">
                     
@@ -71,17 +78,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
                             <td><button>Delete</button>
                         </tr>
                     </table>
-                </div>`  
-                    main_body.appendChild(content) 
-                })
-            })
+                </div>`
+            main_body.appendChild(content)
+        })
+    })
 
-            messageButton.addEventListener('click',e=>{
-                Savedmessage = JSON.parse(localStorage.getItem('messages')) || [];
-                if (main_body.hasChildNodes()) return main_body.innerHTML = "";
-                Savedmessage.forEach(element =>{
-                let content = document.createElement('div');
-                 content.innerHTML=`
+    messageButton.addEventListener('click', e => {
+       // Savedmessage = JSON.parse(localStorage.getItem('messages')) || [];
+       if (main_body.hasChildNodes()) return main_body.innerHTML = "";
+       fetch('https://pink-thankful-oyster.cyclic.app/message')
+        .then(response => response.json())
+        .then(messages => {
+          console.log(messages)
+           messages.data.forEach(element => {
+            let content = document.createElement('div');
+            content.innerHTML = `
               <div class="main-content" id="mainMessage">
               <table>
                   <tr>
@@ -89,88 +100,120 @@ document.addEventListener("DOMContentLoaded", function(event) {
                       <th>Content</th>
                   </tr>
                   <tr>
-                      <td>${(element.email)}</td>
-                      <td>${(element.message)}</td>
+                      <td>${(element.fullname)}</td>
+                      <td>${(element.content)}</td>
                   </tr>
               </table>
           </div>`
-        main_body.appendChild(content)
-                  }  ); })
-
- //bloglist contents
- addBlog=document.getElementById('btn-login');
- addBlog.addEventListener('click', e=> {
- if (document.getElementById('flex-cards').hasChildNodes()) return document.getElementById('flex-cards').innerHTML = "";
- SavedBlog = JSON.parse(localStorage.getItem('blogs'))||[];
- if(!SavedBlog||SavedBlog.length===0){
-    let content=document.createElement('p')
-    content.innerText='No blogs found'
-    document.getElementById('flex-cards').appendChild(content)}
-    else{
-      
- SavedBlog.forEach(element =>{
-    var date=new Date();  
-      var day=date.getDate();  
-      var month=date.getMonth()+1;  
-       var year=date.getFullYear(); 
-     let content = document.createElement('div');
-      content.classList.add('card1');
-      content.innerHTML=`
-      <img src="${(element.image)}" alt="blog" style="width:100%" id="blogImage">
-      <p class="title" id="blogTitle">${(element.title)}</p>
-      <p id="date">Date created:${day}/${month}/${year}</p>
-      <p><button class="update" id="${(element.blogId)}">Edit</button></p><p><button class="delete" id="${element.blogId}">delete</button></p>`
-
-      document.getElementById('flex-cards').appendChild(content)
-      
+            main_body.appendChild(content)
+        });
+        })
         
-      //delete
-      var ButtonDelete=document.querySelectorAll('.delete')
-      var cardToDelete=document.querySelectorAll('.card1')
-     var SavedBlog = localStorage.getItem('blogs');
-     for(let i=0;i<ButtonDelete.length;i++){
-         ButtonDelete[i].addEventListener('click',()=>{
-          
-             id=ButtonDelete[i].getAttribute("id")
-            
-             localStorage.setItem("ToDelete",id)
-            
-             blogs1=JSON.parse(SavedBlog)
-             id=JSON.parse(localStorage.getItem('ToDelete'))
-             let blogCopy=[]
-             blogs1.forEach(element => {
-                if(element.blogId!=id){  
-                    
-                 blogCopy.push(element)
-                }
-               
-              cardToDelete[i].style.display='none'
-             localStorage.setItem("blogs",JSON.stringify(blogCopy))
-            
-            });
-            }
-            
-     )}
-     //update
-     var ButtonUpdate=  document.querySelectorAll('.update');
-     //save blog id in localstorage
-     for(let i=0;i<ButtonUpdate.length;i++){
-         ButtonUpdate[i].addEventListener('click',()=>{
-             var id=ButtonUpdate[i].getAttribute("id")
-             console.log(id);
-             localStorage.setItem("ToUpdate",id)
-             window.location="/BlogEdit.html"
-         })
-        }
+       
+    })
+
+    //bloglist contents
+    addBlog = document.getElementById('btn-login');
+    addBlog.addEventListener('click', e => {
+        // if (document.getElementById('flex-cards').hasChildNodes()) return document.getElementById('flex-cards').innerHTML = "";
+        // SavedBlog = JSON.parse(localStorage.getItem('blogs')) || [];
+        // if (!SavedBlog || SavedBlog.length === 0) {
+        //     let content = document.createElement('p')
+        //     content.innerText = 'No blogs found'
+        //     document.getElementById('flex-cards').appendChild(content)
+        // }
+
+
+        fetch('https://pink-thankful-oyster.cyclic.app/blogs')
+            .then(response => response.json())
+            .then(blogs => {
+                console.log(blogs.data.length)
+                blogs.data.forEach(blog => {
+                    console.log(blog)
+                    var date = new Date();
+                    var day = date.getDate();
+                    var month = date.getMonth() + 1;
+                    var year = date.getFullYear();
+                    let content = document.createElement('div');
+                    content.classList.add('card1');
+                    content.innerHTML = `
+      <img src="${(blog.image)}" alt="blog" style="width:100%" id="blogImage">
+      <p class="title" id="blogTitle">${(blog.title)}</p>
+      <p id="date">Date created:${(blog.createdAt.split("T")[0])}</p>
+      <p><button class="update" id="${(blog._id)}">Edit</button></p><p><button class="delete" id="${blog._id}">delete</button></p>`
+
+                    document.getElementById('flex-cards').appendChild(content)
+
+
+
+                })
+
+            })
+            .catch(error => alert(error))
+        //         SavedBlog.forEach(element => {
+        //             var date = new Date();
+        //             var day = date.getDate();
+        //             var month = date.getMonth() + 1;
+        //             var year = date.getFullYear();
+        //             let content = document.createElement('div');
+        //             content.classList.add('card1');
+        //             content.innerHTML = `
+        //   <img src="${(element.image)}" alt="blog" style="width:100%" id="blogImage">
+        //   <p class="title" id="blogTitle">${(element.title)}</p>
+        //   <p id="date">Date created:${day}/${month}/${year}</p>
+        //   <p><button class="update" id="${(element.blogId)}">Edit</button></p><p><button class="delete" id="${element.blogId}">delete</button></p>`
+
+        //             document.getElementById('flex-cards').appendChild(content)
+
+
+        //             //delete
+        //             var ButtonDelete = document.querySelectorAll('.delete')
+        //             var cardToDelete = document.querySelectorAll('.card1')
+        //             var SavedBlog = localStorage.getItem('blogs');
+        //             for (let i = 0; i < ButtonDelete.length; i++) {
+        //                 ButtonDelete[i].addEventListener('click', () => {
+
+        //                     id = ButtonDelete[i].getAttribute("id")
+
+        //                     localStorage.setItem("ToDelete", id)
+
+        //                     blogs1 = JSON.parse(SavedBlog)
+        //                     id = JSON.parse(localStorage.getItem('ToDelete'))
+        //                     let blogCopy = []
+        //                     blogs1.forEach(element => {
+        //                         if (element.blogId != id) {
+
+        //                             blogCopy.push(element)
+        //                         }
+
+        //                         cardToDelete[i].style.display = 'none'
+        //                         localStorage.setItem("blogs", JSON.stringify(blogCopy))
+
+        //                     });
+        //                 }
+
+        //                 )
+        //             }
+        //             //update
+        //             var ButtonUpdate = document.querySelectorAll('.update');
+        //             //save blog id in localstorage
+        //             for (let i = 0; i < ButtonUpdate.length; i++) {
+        //                 ButtonUpdate[i].addEventListener('click', () => {
+        //                     var id = ButtonUpdate[i].getAttribute("id")
+        //                     console.log(id);
+        //                     localStorage.setItem("ToUpdate", id)
+        //                     window.location = "/BlogEdit.html"
+        //                 })
+        //             }
+
+        //         })
+
+
+
+
+
+    })
+
+
 
 })
-
-
-     } 
-    
-    
-    })
-
-
-
-    })
--- login.js
+++ login.js
@@ -1,92 +1,110 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-const form = document.querySelector('form');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const submit=document.getElementById('submit');
+    const form = document.querySelector('form');
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+    const submit = document.getElementById('submit');
 
 
-form.addEventListener('submit', e=> {
-e.preventDefault();
-validateInputs();
- loginFunction();
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        validateInputs();
+        loginFunction();
 
-});
-const setError =(element,message)=>{
-    inputControl=element.parentElement;
-    const errorDisplay=inputControl.querySelector('.error');
-    errorDisplay.innerText= message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success');
-    return true;
-}
-const setSuccess=element=>{
-    
-    const inputControl= element.parentElement;
-    const errorDisplay=inputControl.querySelector('.error');
-    errorDisplay.innerText="";
-    inputControl.classList.remove('error');
-    return true;
-    // inputControl.classList.add('success');
-}
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-const validateInputs=()=>{
-    let usernameValue=username.value.trim();
-    let passwordValue=password.value.trim();
-    let successUsername
-    let successPassword
-    if(usernameValue ===''){
-        setError(username,"username is required")
+    });
+    const setError = (element, message) => {
+        inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+        errorDisplay.innerText = message;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success');
+        return true;
     }
-    else if(!isValidEmail(usernameValue)){
-        setError(username,"provide a valid email address")
-    }
-    else{
-     successUsername= setSuccess(username);
-    }
-    if(passwordValue===''){
-        setError(password,"password is required")   
-    }
-    else{
-        successPassword=setSuccess(password);
-    }
-// if(successUsername&&successPassword){
-//     loginFunction();
-// }
-}
-const togglePassword = document.querySelector('#togglePassword');
-  togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('bi-eye');
-    
-});
-function loginFunction(e){
-   
-    const users=localStorage.getItem('users')||[];
-    
-    Arrusers=JSON.parse(users)
-    // console.log(typeof(Arrusers))
-    
-    usermail=username.value.trim()
-    userpass=password.value.trim()
-    const targetUser= Arrusers.find(users=>users.email==usermail);
-    if(targetUser&& targetUser.password==userpass){
-        window.location="/dashboard.html"
-        localStorage.setItem('currentUser', JSON.stringify(targetUser))
-    }
-    else if(targetUser&& targetUser.password!=userpass){
-        setError(password,"incorrect password")    
-    }
-    else{
-        setError(username,"user not found,try signing up") 
-        password.value=''
-    }}
+    const setSuccess = element => {
 
-});
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+        errorDisplay.innerText = "";
+        inputControl.classList.remove('error');
+        return true;
+        // inputControl.classList.add('success');
+    }
+    const isValidEmail = email => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+    const validateInputs = () => {
+        let usernameValue = username.value.trim();
+        let passwordValue = password.value.trim();
+        let successUsername
+        let successPassword
+        if (usernameValue === '') {
+            setError(username, "username is required")
+        }
+        else if (!isValidEmail(usernameValue)) {
+            setError(username, "provide a valid email address")
+        }
+        else {
+            successUsername = setSuccess(username);
+        }
+        if (passwordValue === '') {
+            setError(password, "password is required")
+        }
+        else {
+            successPassword = setSuccess(password);
+        }
+        // if(successUsername&&successPassword){
+        //     loginFunction();
+        // }
+    }
+    const togglePassword = document.querySelector('#togglePassword');
+    togglePassword.addEventListener('click', function (e) {
+        // toggle the type attribute
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+        // toggle the eye slash icon
+        this.classList.toggle('bi-eye');
 
+    });
+    function loginFunction(e) {
+        usermail = username.value
+        userpass = password.value
+
+        const data = { email: usermail, password: userpass };
+        console.log(JSON.stringify(data));
+        fetch('https://pink-thankful-oyster.cyclic.app/login', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then((response) =>
+                response.json()
+            )
+            .then((data) => {
+              if((data.message).includes("invalid")){
+                console.log(data.message)
+                setError(username, "invalid login credentials")
+                setError(password, "invalid login credentials")
+              }
+              else if((data.message).includes("required")){
+                setError(username, data.message)
+              }
+              else if((data.message).includes("valid")){
+                setError(username, data.message)
+              }
+              else{
+                console.log(data.message)
+                localStorage.setItem('token', data.token)
+                window.location = '/dashboard.html'
+              }
+                
+
+            })
+            .catch(error => alert(error))
+        console.log(data)
+
+
+    }
+})
